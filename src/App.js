675d1da5699222fed886aaa9a62725d2b3182bfa
Ahn <a106280@gmail.com>
2024-01-03 13:57:12 +0900

    section layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
 import Home from './pages/Home'
 import Today  from './pages/Today'
 import Developer from './pages/Developer'
@@ -16,20 +19,24 @@ import Not from './pages/Not'
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/today' element={<Today/>}/>
-                <Route path='/developer' element={<Developer/>}/>
-                <Route path='/webd' element={<Webd/>}/>
-                <Route path='/website' element={<Website/>}/>
-                <Route path='/gsap' element={<Gsap/>}/>
-                <Route path='./port' element={<Port/>}/>
-                <Route path='/youtube' element={<Youtube/>}/>
-                <Route path='/channel/:channelID' element={<Channel/>}/>
-                <Route path='/video/:videoID' element={<Video/>}/>
-                <Route path='/search/:searchID' element={<Search/>}/>
-                <Route path='/*' element={<Not/>}/>
-            </Routes>
+            <Header />
+            <Main >
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/today' element={<Today/>}/>
+                    <Route path='/developer' element={<Developer/>}/>
+                    <Route path='/webd' element={<Webd/>}/>
+                    <Route path='/website' element={<Website/>}/>
+                    <Route path='/gsap' element={<Gsap/>}/>
+                    <Route path='./port' element={<Port/>}/>
+                    <Route path='/youtube' element={<Youtube/>}/>
+                    <Route path='/channel/:channelID' element={<Channel/>}/>
+                    <Route path='/video/:videoID' element={<Video/>}/>
+                    <Route path='/search/:searchID' element={<Search/>}/>
+                    <Route path='/*' element={<Not/>}/>
+                </Routes>
+            </ Main>
+            <Footer />
         </BrowserRouter>
     )
 }
